7be362a142e596c2ff2b131520268fcb09a4792f
YaTut1901 <ovanya1901@gmail.com>
2024-03-11 16:02:32 +0000

    "solidity-11"

---
+++ solidity-0.8-tasks/solidity-11/Constants.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+// get addr costs 351 gas
+contract Constants {
+    address public constant addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+}
+
+// get addr cossts 2483 gas
+contract Variables {
+    address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+}
